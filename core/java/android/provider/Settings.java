9e5eed70e29fb0706746cd0f8315162b60e5e1e7
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-25 14:47:01 +0000

    SystemUI: Add haptics to brightness slider [1/2]
    Improves the UX while dragging the brightness slider.
    
    Squashed with:
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sun Jun 18 15:39:24 2023 +0000
    
        Add toggle to enable brightness slider haptic feedback [1/2]
    
        Change-Id: I7b5ffcf8a44427a33f0f33bebee294cd47007afe
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: SamarV-121 <samar@samarv121.dev>
    Date:   Tue Feb 20 09:56:13 2024 +0530
    
        SystemUI: Improve brightness slider haptics
    
        - Improve hasVibrator checks
        - Switch to TEXTURE_TICK effect
        - Give haptic feedback on brightness slider only after 5 changes for moarr subtleness
          REF: https://review.statixos.com/c/android_frameworks_base/+/9392/12#message-5d4d08b55de6103928b06144e321cf9af0463e34
    
        Change-Id: I2b99a20e5c5b0b07781bbf7d68829f359c23e162
    
    Author: Anushek Prasal <anushekprasal@gmail.com>
    Date:   Wed Jun 28 23:45:02 2023 +0530
    
        SystemUI: Add haptic feedback to auto brightness icon
    
        Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    
    Co-authored-by: SamarV-121 <samar@samarv121.dev>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I79d9d8fd1133a9e166821ff8f7135ccb761edd58
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6272,6 +6272,13 @@ public final class Settings {
         public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
 
         /**
+         * Give haptic feedback on qs brightness slider changes.
+         * @hide
+         */
+        @Readable
+        public static final String QS_BRIGHTNESS_SLIDER_HAPTIC_FEEDBACK = "qs_brightness_slider_haptic_feedback";
+
+        /**
          * Give haptic feedback on volume slider changes.
          * @hide
          */
